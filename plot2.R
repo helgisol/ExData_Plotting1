36057c00be8911b4e8db7ce396780f6c70831cf4
Oleg Semery <semery@jupiter.cvision.lab>
2015-11-11 15:51:29 +0300

    Add comments and data auto-loading

--- plot2.R
+++ plot2.R
@@ -1,6 +1,29 @@
+# Plotting Time Series.
+
+# Obtaining raw data.
+obtainRawData <- function(dataFileName) {
+  
+  if (!file.exists(dataFileName)) {
+    
+    dataUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    zipFileName <- "exdata-data-household_power_consumption.zip"
+    download.file(dataUrl, zipFileName, mode="wb")
+    unzip(zipFileName)
+    unlink(zipFileName)
+    TRUE
+  }
+  else {
+    
+    FALSE
+  }
+}
+
+# Read source data.
 readData <- function() {
   
-  data <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))  
+  dataFileName <- "household_power_consumption.txt";
+  obtainRawData(dataFileName);
+  data <- read.csv(dataFileName, sep=";", na.strings="?", colClasses = c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))  
   data$Date <- as.Date(data$Date,"%d/%m/%Y")
   data <- data[data$Date >= as.Date("2007-02-01") & data$Date <= as.Date("2007-02-02"),]
   data$Time <- strptime(paste(as.character(data$Date),data$Time), format="%Y-%m-%d %H:%M:%S")
